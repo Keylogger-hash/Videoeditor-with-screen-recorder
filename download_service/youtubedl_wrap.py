b8533d2d056f1b9df44d857cce2d2014a34f9b02
Kirill Anikin <akir@mmvs.ru>
2021-10-18 12:59:29 +0500

    Changed downloading process loop

--- download_service/youtubedl_wrap.py
+++ download_service/youtubedl_wrap.py
@@ -1,34 +1,30 @@
 from threading import Event
-from myyoutube_dl import YoutubeDl
+from download_service.myyoutube_dl import YoutubeDl
 import subprocess
 
 dls_opt = {"ratelimit": 1000000000}
 
-youtubedl = YoutubeDl()
+
 
 
 def download_video(link: str, destination: str, fire_exit: Event = None, start_callback=None):
     if start_callback is not None:
         start_callback()
-
-    proc = youtubedl.input(link).global_args("--output", destination, "-f", 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best').run(wait=False)
+    youtubedl = YoutubeDl()
+    proc = youtubedl.input(link).global_args("-q", "--output", destination, "-f", 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best').run(wait=False)
     print(proc)
 
     while fire_exit is None or not fire_exit.is_set():
-        output = proc.stdout.readline()
-        print(output)
-        if output == b'' and proc.poll() is not None:
+        # wait for 10s and break if process is terminated
+        try:
+            proc.wait(10)
+            print('Process stopped')
             break
+        except subprocess.TimeoutExpired:
+            pass
 
     if fire_exit is None or not fire_exit.is_set():
         return proc.poll()
     else:
         proc.terminate()
-
-
-def stop_download_video(link: str, fire_exit: Event=None):
-    proc = youtubedl.input(link).stop()
-    print(proc.poll())
-    if fire_exit is None or not fire_exit.is_set():
-        return proc.poll()
-
+        proc.wait() # wait until process terminates completely
