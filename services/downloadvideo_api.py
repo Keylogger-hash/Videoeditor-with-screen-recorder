1674b4717af1f7cc96f8f346e5a4b3d216a587d8
Pavel <pvvolo2200@gmail.com>
2021-11-19 13:43:46 +0500

    Return youtube_dl because yt_dlp return only android url

--- services/downloadvideo_api.py
+++ services/downloadvideo_api.py
@@ -8,6 +8,7 @@ from flask import Blueprint, current_app, request
 from database.datamodel import videos, download_videos
 import uuid
 import yt_dlp
+import youtube_dl
 from base64 import b32encode
 import requests
 from settings import DOWNLOADS_LOCATION
@@ -215,7 +216,7 @@ def start_downloading():
     format_ext = 'mp4'
     filesize = 0
     if result is None:
-        video_info = yt_dlp.YoutubeDL().extract_info(data['link'], download=False)
+        video_info = youtube_dl.YoutubeDL().extract_info(data['link'], download=False)
         title = video_info['title']
         for variant in video_info['formats']:
             if variant['format_id'] == format_id:
