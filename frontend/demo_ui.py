230cae219d7a98d687ae1788e7f2dd8fa1f20991
Kirill Anikin <akir@mmvs.ru>
2021-11-22 14:15:41 +0500

    Added player page, added share link to cut video modals

--- frontend/demo_ui.py
+++ frontend/demo_ui.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, render_template, url_for, send_from_directory
 
-demo_ui = Blueprint('demo_frontend', __name__)
+demo_ui = Blueprint('demo_frontend', __name__, template_folder='html')
 
 @demo_ui.get('/')
 def upload_page():
@@ -13,6 +13,10 @@ def editor_page():
 @demo_ui.get('/results')
 def results_page():
     return send_from_directory('frontend/html', 'results.html')
+
+@demo_ui.get('/play/<output_name>')
+def player_page(output_name):
+    return render_template('player.html', output_name=output_name)
 
 @demo_ui.get('/static/<path>')
 def serve_static_assets(path):
